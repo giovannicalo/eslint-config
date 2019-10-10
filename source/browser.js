c80fa28612cbaa8ce246de1c2c745339409d5c45
Giovanni Calò <giovanni.calo@live.it>
2019-10-10 16:21:57 +0100

    Added Node environment override for Webpack configuration

--- source/browser.js
+++ source/browser.js
@@ -5,6 +5,10 @@ module.exports = {
 		jquery: true
 	},
 	extends: "./index.js",
+	overrides: [{
+		env: { node: true },
+		files: ["webpack.config.js"]
+	}],
 	rules: {
 		"import/no-nodejs-modules": 2,
 		"no-console": 1
